c760a18732c29fffe9916c0bd2c3dc58811ec214
Lau Skeeter <lawr@lauragonzalez.cc>
2019-08-13 13:03:59 +0100

    fixesz

--- w/gradient/paintworklet.js
+++ w/gradient/paintworklet.js
@@ -2,22 +2,37 @@ registerPaint(
 	'bg',
 	class {
 		static get inputProperties() {
-			return ['--from', '--to'];
+			return ['--bg', '--dots', '--radius'];
 		}
 		paint(ctx, geom, props) {
-			const [from, to] = [
-				props.get('--from').toString(),
-				props.get('--to').toString(),
+			const [bg, dots, radius] = [
+				props.get('--bg').toString(),
+				props.get('--dots').toString(),
+				props.get('--radius') || 20,
 			];
-
-			const gradient = ctx.createLinearGradient(0, 0, geom.width, geom.height);
-			gradient.addColorStop(0, from);
-			gradient.addColorStop(1, to);
-
-			ctx.fillStyle = gradient;
-
+			ctx.fillStyle = bg;
 			ctx.fillRect(0, 0, geom.width, geom.height);
+
+			ctx.fillStyle = dots;
+			for (let row = -1; row < 12; row++) {
+				for (let column = -1; column < 12; column++) {
+					ctx.arc(
+						(geom.width / 10) * (row + (column % 2) * 0.5),
+						(geom.height / 10) * column,
+						geom.width / radius,
+						0,
+						2 * Math.PI
+					);
+					ctx.closePath();
+				}
+			}
+			ctx.fill();
 		}
 		fill;
 	}
 );
+/*
+			ctx.fillStyle = 'black';
+			ctx.globalCompositeOperation = 'xor';
+
+*/
