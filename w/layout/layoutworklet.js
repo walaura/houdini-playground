5028e516ce4e04a5a10798cae654e5471e37b409
Lau Skeeter <lawr@lauragonzalez.cc>
2019-11-21 09:39:40 +0000

    Update layoutworklet.js

--- w/layout/layoutworklet.js
+++ w/layout/layoutworklet.js
@@ -1,17 +1,6 @@
 registerLayout(
 	"center",
 	class {
-		*intrinsicSizes(children) {
-			const childrenSizes = yield children.map(child => {
-				return child.intrinsicSizes();
-			});
-
-			const maxContentSize = childrenSizes.reduce((max, childSizes) => Math.max(max, childSizes.maxContentSize), 0);
-			const minContentSize = childrenSizes.reduce((max, childSizes) => Math.max(max, childSizes.minContentSize), 0);
-
-			return { maxContentSize, minContentSize };
-		}
-
 		async layout(children, edges, constraints) {
 			/* width */
 			const availableInlineSize = constraints.fixedInlineSize;
@@ -25,17 +14,14 @@ registerLayout(
 			}
 
 			/* lets do some layout!! */
-			let offset = 0;
-			for (let fragment of childFragments) {
-				fragment.blockOffset = (availableBlockSize - fragment.blockSize) / 2 + offset;
-				fragment.inlineOffset = (availableInlineSize - fragment.inlineSize) / 2;
-				offset += fragment.blockSize;
-			}
+			/* here lol */
 
 			return {
 				childFragments
 			};
 		}
+
+		async intrinsicSizes() {}
 	}
 );
 
